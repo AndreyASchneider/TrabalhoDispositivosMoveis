127e4fcd8f4f9d21eac489034a8287d5fc52bc53
Andrey <aschneiderpro1@gmail.com>
2024-07-01 23:15:50 -0300

    Adicionando parte com maps

--- app/src/main/java/com/example/codigomorse/GameActivity.java
+++ app/src/main/java/com/example/codigomorse/GameActivity.java
@@ -256,4 +256,10 @@ public class GameActivity extends AppCompatActivity {
         intent.putParcelableArrayListExtra("itemList", new ArrayList<>(enableLetterItemList));
         resultLauncher.launch(intent);
     }
+
+    public void onSaveScore(View view) {
+        Intent intent = new Intent(this, SaveScoreActivity.class);
+        intent.putParcelableArrayListExtra("itemList", new ArrayList<>(enableLetterItemList));
+        resultLauncher.launch(intent);
+    }
 }
--- app/src/main/java/com/example/codigomorse/SaveScoreActivity.java
+++ app/src/main/java/com/example/codigomorse/SaveScoreActivity.java
@@ -1,24 +1,99 @@
 package com.example.codigomorse;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SaveScoreActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.Locale;
+
+public class SaveScoreActivity extends AppCompatActivity implements LocationListener {
+
+    TextView txtView_location_value;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_save_score);
+
+        txtView_location_value = findViewById(R.id.location_value);
+
+        if (ContextCompat.checkSelfPermission(SaveScoreActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(SaveScoreActivity.this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+
+        getLocation();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, SaveScoreActivity.this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Toast.makeText(this, location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+
+        try {
+            Geocoder geocoder = new Geocoder(SaveScoreActivity.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            String address = addresses.get(0).getAddressLine(0);
+
+            txtView_location_value.setText("testando 123");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
     }
 }
